18c842ba7fead13dcf487c363b6363d2aebec1b5
hus19 <hus19@mails.tsinghua.edu.cn>
2020-02-24 12:08:04 +0800

    Update Some_concept.v
    adder

--- Some_concept.v
+++ Some_concept.v
@@ -1,11 +1,26 @@
 //Verilog 数
- 1'b0  //
- 'b0   //
- 32'b0 //
- 4'ha  //4
- 5'h5  //
- 4'hz  //4
+ 1'b0  // 1位，值全为0
+ 'b0   // 32位
+ 32'b0 //32位
+ 4'ha  //4位十六进制数1010
+ 5'h5  //5位十六进位数00101
+ 4'hz  //zzzz
  4'h? zz?  //
  4'bx  //xxxx
  9  //32位数（从左边开始，前28位用0填充）
  a //非法数 
+ 
+ //====================简单加法器的代码===========//
+ module adder (clock,reset,a,b,c);
+	 input clock,reset,a,b;
+	 reg[1:0]c;
+	 always @(posedge clock or posedge reset)
+		begin if (reset)
+		c = 2'b0;
+		else
+		c = a+b;
+	end
+end module
+ //========简单加法布尔等式==========//
+ c[0] <= a^b; //verilog布尔逻辑XOR运算符
+ c[1] <= a&b; //verilog布尔逻辑AND运算符
